72be8c06179ac7d5a2e99eb7992bd8808ff556f2
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-06 16:35:16 -0500

    Cleaning up code and added a constructor for best practices.

--- client/src/AppWithoutHooks.js
+++ client/src/AppWithoutHooks.js
@@ -1,10 +1,16 @@
 import React, { Component } from 'react';
 
 class AppWithoutHooks extends Component {
-    state = {
+    constructor(props) {
+        super(props);
+
+
+        this.state = {
         characters: null,
         loading: true
     }
+    }
+    
 
     async componentDidMount() {
         const response = await fetch("http://localhost:5000/test")
@@ -18,10 +24,12 @@ class AppWithoutHooks extends Component {
     }
     
     render() {
+        const { state } = this;
+        const { characters, loading } = state
         return (
             <div>
                 {
-                    this.state.loading ? <p>Loading...</p> : this.state.characters.map((singleCharacter) => {
+                    loading ? <p>Loading...</p> : characters.map((singleCharacter) => {
                         const { id, name: characterName } = singleCharacter;
                         return (
                             <p key={id}>{characterName}</p>
