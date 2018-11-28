faaa8076441a92336abc5e9223e785b1f3d0bed0
Domitrius Clark <domitriusaclark@gmail.com>
2018-11-28 16:31:38 -0500

    Changed to track sass and not regular css.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import './main.scss';
 import AppWithoutHooks from './AppWithoutHooks'
 import AppWithHooks from './AppWithHooks';
 
