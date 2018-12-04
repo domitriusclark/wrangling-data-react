9dff087ac61eae661eae4c8cbfd5d7e430a30881
Domitrius Clark <domitriusaclark@gmail.com>
2018-12-04 03:12:50 -0500

    Removed tests for meetup.

--- client/src/components/CharacterPage/__tests__/CharacterPage.test.js
+++ client/src/components/CharacterPage/__tests__/CharacterPage.test.js
@@ -4,33 +4,11 @@ import { MockedProvider, contextValueSeed } from '../../../utils/test-utils';
 
 import CharacterPage from '../CharacterPage';
 
-
-
 test('Character Page renders with no error', () => {
-    const tree = (
-        <MockedProvider value={contextValueSeed}>
-            <CharacterPage />
-        </MockedProvider>
-    );
-    render(tree);    
-
-    const characterPage = document.getElementsByClassName('CharacterPage')[0]
-
-    expect(characterPage).toBeDefined();
 
 });
 
 test('Character Page renders a CharacterCard for every character on state', () => {
-    const tree = (
-        <MockedProvider value={contextValueSeed}>
-            <CharacterPage />
-        </MockedProvider>
-    );
-    render(tree);  
-    
-    const characterPage = document.getElementsByClassName('CharacterPage')[0]
-    
-    expect(characterPage.children).toHaveLength(contextValueSeed.state.characters.length);
 
 });
 
--- client/src/components/MainPage/__tests__/MainPage.test.js
+++ client/src/components/MainPage/__tests__/MainPage.test.js
@@ -6,40 +6,12 @@ import MainPage from '../MainPage';
 import ShowCharactersButton from '../../ShowCharactersButton/ShowCharactersButton'
 import CharacterPage from '../../CharacterPage/CharacterPage';
 
-const MockedProvider = MyContext.Provider;
-
-const mockedCharacter = [
-    {id: 1, name: 'wow', stats: {hp: 1}}
-];
-
-const showCharacters = jest.fn();
-
 test('MainPage renders ShowCharactersButton when state.characters is null', () => {
-    const tree = (
-        <MockedProvider value={{state: {characters: null}, actions: showCharacters }}>
-            <MainPage WithoutCharacters={ShowCharactersButton} /> }    
-        </MockedProvider>
-    );
-    
-    render(tree)
 
-    const charactersButton = document.getElementsByClassName('ShowCharactersButton')[0]
-
-    expect(charactersButton).toBeDefined();
 });
 
 test('MainPage renders CharacterPage when supplied data', () => {
-    const tree = (
-        <MockedProvider value={{state: { characters: mockedCharacter }}}>
-            <MainPage WithCharacters={CharacterPage} />
-        </MockedProvider>
-    );
 
-    render(tree);
-
-    const characterPage = document.getElementsByClassName('CharacterPage')[0];
-
-    expect(characterPage).toBeDefined();
 });
 
 
