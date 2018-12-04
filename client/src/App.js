3a156108c45c56f4a83985297bffea8ef363fc8f
Domitrius Clark <domitriusaclark@gmail.com>
2018-12-04 13:01:46 -0500

    Changed Provider to be injected with fetch call.

--- client/src/App.js
+++ client/src/App.js
@@ -7,11 +7,17 @@ import CharacterPage from './components/CharacterPage/CharacterPage';
 
 
 class App extends Component {
+    async fetchData() {
+        const response = await fetch("http://localhost:5000/test");
+        return await response.json();
+    }
+
     render() {
+        
         return (
             <div className="App">
-                <Provider>
-                    <MainPage WithCharacters={CharacterPage} WithoutCharacters={ShowCharactersButton} />
+                <Provider data={this.fetchData.bind(this)}>
+                    <MainPage fetchData={this.fetchData.bind(this)} WithCharacters={CharacterPage} WithoutCharacters={ShowCharactersButton} />
                 </Provider>                
             </div>
         )
