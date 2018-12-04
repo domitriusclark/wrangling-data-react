8a9202c59ac297f3fca3643990d6454c2e8ce69a
Domitrius Clark <domitriusaclark@gmail.com>
2018-12-04 03:07:51 -0500

    Took tests out.

--- client/src/components/CharacterPage/__tests__/CharacterPage.test.js
+++ client/src/components/CharacterPage/__tests__/CharacterPage.test.js
@@ -7,30 +7,10 @@ import CharacterPage from '../CharacterPage';
 
 
 test('Character Page renders with no error', () => {
-    const tree = (
-        <MockedProvider value={contextValueSeed}>
-            <CharacterPage />
-        </MockedProvider>
-    );
-    render(tree);    
-
-    const characterPage = document.getElementsByClassName('CharacterPage')[0]
-
-    expect(characterPage).toBeDefined();
-
+    
 });
 
 test('Character Page renders a CharacterCard for every character on state', () => {
-    const tree = (
-        <MockedProvider value={contextValueSeed}>
-            <CharacterPage />
-        </MockedProvider>
-    );
-    render(tree);  
-    
-    const characterPage = document.getElementsByClassName('CharacterPage')[0]
-    
-    expect(characterPage.children).toHaveLength(contextValueSeed.state.characters.length);
 
 });
 
--- client/src/components/MainPage/__tests__/MainPage.test.js
+++ client/src/components/MainPage/__tests__/MainPage.test.js
@@ -8,31 +8,11 @@ import { MockedProvider, contextValueSeed, defaultState } from '../../../utils/t
 
 
 test('MainPage renders ShowCharactersButton when state.characters is null', () => {
-    const tree = (
-        <MockedProvider value={defaultState}>
-            <MainPage WithoutCharacters={ShowCharactersButton} /> }    
-        </MockedProvider>
-    );
-    
-    render(tree)
 
-    const charactersButton = document.getElementsByClassName('ShowCharactersButton')[0]
-
-    expect(charactersButton).toBeDefined();
 });
 
 test('MainPage renders CharacterPage when supplied data', () => {
-    const tree = (
-        <MockedProvider value={contextValueSeed}>
-            <MainPage WithCharacters={CharacterPage} />
-        </MockedProvider>
-    );
 
-    render(tree);
-
-    const characterPage = document.getElementsByClassName('CharacterPage')[0];
-
-    expect(characterPage).toBeDefined();
 });
 
 
