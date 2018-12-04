536f458e6ef5f229f8724a71044913120aabf01e
Domitrius Clark <domitriusaclark@gmail.com>
2018-12-04 03:11:17 -0500

    accidentally removed tests from the wrong branch.

--- client/src/components/MainPage/__tests__/MainPage.test.js
+++ client/src/components/MainPage/__tests__/MainPage.test.js
@@ -7,12 +7,33 @@ import CharacterPage from '../../CharacterPage/CharacterPage';
 import { MockedProvider, contextValueSeed, defaultState } from '../../../utils/test-utils';
 
 
-test('MainPage renders ShowCharactersButton when state.characters is null', () => {
 
+test('MainPage renders ShowCharactersButton when state.characters is null', () => {
+    const tree = (
+        <MockedProvider value={defaultState}>
+            <MainPage WithoutCharacters={ShowCharactersButton} /> }    
+        </MockedProvider>
+    );
+    
+    render(tree)
+
+    const charactersButton = document.getElementsByClassName('ShowCharactersButton')[0]
+
+    expect(charactersButton).toBeDefined();
 });
 
 test('MainPage renders CharacterPage when supplied data', () => {
+    const tree = (
+        <MockedProvider value={contextValueSeed}>
+            <MainPage WithCharacters={CharacterPage} />
+        </MockedProvider>
+    );
 
+    render(tree);
+
+    const characterPage = document.getElementsByClassName('CharacterPage')[0];
+
+    expect(characterPage).toBeDefined();
 });
 
 
--- client/src/components/ShowCharactersButton/__tests__/ShowCharactersButton.test.js
+++ client/src/components/ShowCharactersButton/__tests__/ShowCharactersButton.test.js
@@ -5,6 +5,17 @@ import ShowCharactersButton from '../ShowCharactersButton';
 import { MockedProvider, contextValueSeed } from '../../../utils/test-utils';
 
 test('ShowCharactersButton renders to the DOM with no error', () => {
+    const tree = (
+        <MockedProvider value={contextValueSeed}>
+            <ShowCharactersButton />
+        </MockedProvider>
+    );
+    const { getByText } = render(tree);
+
+    const characterButton = getByText('Show a list of characters!');
+
+    fireEvent.click(characterButton);
+
+    expect(characterButton).toBeDefined();
 
 });
-
