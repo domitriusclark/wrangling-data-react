612ad48ee60e3bc486f15b62b15927615aa625d7
Domitrius Clark <domitriusaclark@gmail.com>
2018-12-04 13:18:59 -0500

    Changed the Provider to accept the fetch on props.

--- client/src/components/Provider/Provider.js
+++ client/src/components/Provider/Provider.js
@@ -3,15 +3,20 @@ import React, { Component } from 'react';
 export const MyContext = React.createContext();
 
 export class Provider extends Component {
-    state = {
-        characters: null,
-        loading: true,
-        name: 'Domitrius'
-    };
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            characters: null,
+            loading: true,
+            name: 'Domitrius',
+            fetchCharacters: props.data
+        }
+    }
+
 
     showCharacters = async () => {
-        const response = await fetch("http://localhost:5000/test")
-        const data = await response.json();
+        const data = await this.state.fetchCharacters();
         const {characters} = data;
         const classObjectToArray = Object.keys(characters).map(character => characters[character]);
         this.setState({
@@ -27,7 +32,7 @@ export class Provider extends Component {
                     state: this.state,
                     actions: {
                         showCharacters: this.showCharacters
-                    }                    
+                    }
                 }}
             >
                 {this.props.children}
